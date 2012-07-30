ba3e327d41c3ff0351a458d641d90aabdeb28762
ahocevar <andreas.hocevar@gmail.com>
2012-07-30 08:58:17 +0200

    [coding style] Shorter functions for readability.

--- src/script/tl/Renderer.js
+++ src/script/tl/Renderer.js
@@ -19,13 +19,8 @@ tl.extend(tl.Renderer.prototype, {
             tl.cancelAnimationFrame(me.animationId);
         }
         me.animationId = tl.requestAnimationFrame(function() {        
-            var fragment = document.createDocumentFragment(),
-                centerX = (bounds.maxX + bounds.minX) / 2,
-                centerY = (bounds.maxY + bounds.minY) / 2,
-                layerData, tileDelta, tile, image, offsetX, offsetY,
-                insertAt, width, height, hash, queue, distanceFromCenter,
-                reposition = false,
-                keep = [];
+            var keep = [],
+                queue = [];
             if (resolution !== me.resolution) {
                 me.resolution = resolution;
                 me.bounds = null;
@@ -41,49 +36,9 @@ tl.extend(tl.Renderer.prototype, {
                     "translate(" + me.left + "px," + me.top + "px)"},
                 {left: me.left + "px", top: me.top + "px"}
             );
-            queue = [];
             for (var l=0, ll=data.length; l<ll; ++l) {
-                layerData = data[l];
-                tileDelta = layerData.tileDelta;
-                insertAt = layerData.insertAt;
-                offsetX = (insertAt.x - bounds.minX) / resolution - me.left;
-                offsetY = (bounds.maxY - insertAt.y) / resolution - me.top;
-                width =  tileDelta.x / resolution;
-                height = tileDelta.y / resolution;
-                for (var i=0, ii=layerData.data.length; i<ii; ++i) {
-                    for (var j=0, jj=layerData.data[i].length; j<jj; ++j) {
-                        tile = layerData.data[i][j];
-                        image = tile.image;
-                        if (!~tl.indexOf(me.renderedTiles, tile)) {
-                            distanceFromCenter = Math.sqrt(
-                                Math.pow(insertAt.x + (i+0.5) * tileDelta.x - centerX, 2) +
-                                Math.pow(insertAt.y - (j+0.5) * tileDelta.y - centerY, 2)
-                            );
-                            queue.push([tile, distanceFromCenter]);
-                            fragment.appendChild(image);
-                            reposition = true;
-                        }
-                        hash = [resolution, insertAt.x, insertAt.y].join(',');
-                        if (hash !== tile.hash) {
-                            /* store resolution and insert offsets - if any is
-                            different, width and height need to be recalculated
-                            to avoid gaps between tiles */
-                            tile.hash = hash;
-                            reposition = true;
-                            image.style.width = (Math.round(offsetX + (i+1) * width) - Math.round(offsetX + i * width)) + 'px';
-                            image.style.height = (Math.round(offsetY + (j+1) * height) - Math.round(offsetY + j * height)) + 'px';
-                        }
-                        if (reposition) {
-                            reposition = false;
-                            image.style.left = Math.round(offsetX + i * width) + 'px';
-                            image.style.top = Math.round(offsetY + j * height) + 'px';                            
-                        }
-                        keep.push(tile);
-                    }
-                }
+                me.processTiles(data[l], bounds, resolution, keep, queue);
             }
-            me.loadTiles(queue);
-            me.el.appendChild(fragment);
             var rendered, r;
             for (r=me.renderedTiles.length-1; r>=0; --r) {
                 rendered = me.renderedTiles[r];
@@ -91,23 +46,72 @@ tl.extend(tl.Renderer.prototype, {
                     me.el.removeChild(rendered.image);
                 }
             }
+            me.loadTiles(queue);
             me.bounds = bounds;
             me.renderedTiles = keep;
             me.animationId = null;
         });
     },
-    loadTiles: function(queue) {
-        queue.sort(this.sortTiles);
-        for (var i=0, ii=queue.length; i<ii; ++i) {
-            this.loadTile(queue[i][0], -~((i / 4) | 0) * 10);
+    processTiles: function(layerData, bounds, resolution, keep, queue) {
+        var me = this,
+            tileDelta = layerData.tileDelta,
+            insertAt = layerData.insertAt,
+            centerX = (bounds.maxX + bounds.minX) / 2,
+            centerY = (bounds.maxY + bounds.minY) / 2,
+            offsetX = (insertAt.x - bounds.minX) / resolution - me.left,
+            offsetY = (bounds.maxY - insertAt.y) / resolution - me.top,
+            width =  tileDelta.x / resolution,
+            height = tileDelta.y / resolution,
+            tile, image, hash, reposition, distanceFromCenter;
+        for (var i=0, ii=layerData.data.length; i<ii; ++i) {
+            for (var j=0, jj=layerData.data[i].length; j<jj; ++j) {
+                tile = layerData.data[i][j];
+                image = tile.image;
+                if (!~tl.indexOf(me.renderedTiles, tile)) {
+                    distanceFromCenter = Math.sqrt(
+                        Math.pow(insertAt.x + (i+0.5) * tileDelta.x - centerX, 2) +
+                        Math.pow(insertAt.y - (j+0.5) * tileDelta.y - centerY, 2)
+                    );
+                    queue.push([tile, distanceFromCenter]);
+                    reposition = true;
+                }
+                hash = [resolution, insertAt.x, insertAt.y].join(',');
+                if (hash !== tile.hash) {
+                    /* store resolution and insert offsets - if any is
+                    different, width and height need to be recalculated
+                    to avoid gaps between tiles */
+                    tile.hash = hash;
+                    reposition = true;
+                    image.style.width = (Math.round(offsetX + (i+1) * width) - Math.round(offsetX + i * width)) + 'px';
+                    image.style.height = (Math.round(offsetY + (j+1) * height) - Math.round(offsetY + j * height)) + 'px';
+                }
+                if (reposition) {
+                    reposition = false;
+                    image.style.left = Math.round(offsetX + i * width) + 'px';
+                    image.style.top = Math.round(offsetY + j * height) + 'px';                            
+                }
+                keep.push(tile);
+            }
         }
+    },
+    loadTiles: function(queue) {
+        var me = this,
+            fragment = document.createDocumentFragment(),
+            tile;
+        queue.sort(me.sortTiles);
+        for (var i=0, ii=queue.length; i<ii; ++i) {
+            tile = queue[i][0];
+            me.loadTile(tile, -~((i / 4) | 0) * 10);
+            fragment.appendChild(tile.image);
+        }
+        me.el.appendChild(fragment);
     },
     sortTiles: function(a, b) {
         return a[1] - b[1];
     },
     loadTile: function(tile, delay) {
-        tile.timestamp = new Date().getTime();
         var me = this;
+        tile.timestamp = new Date().getTime();
         window.setTimeout(function() {
             if (tile.timestamp >= me.timestamp) {
                 var image = tile.image;
